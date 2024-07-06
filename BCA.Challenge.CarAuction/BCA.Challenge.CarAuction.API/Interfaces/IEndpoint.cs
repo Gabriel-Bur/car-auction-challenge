6e1b56c4fd5c1898016804bbfed5ee2d71ab7892
Gabriel Bur <gabriel.bur@outlook.com>
2024-07-07 00:50:14 +0100

    adding IEndpoint interface + implementing auction service

---
+++ BCA.Challenge.CarAuction/BCA.Challenge.CarAuction.API/Interfaces/IEndpoint.cs
@@ -0,0 +1,7 @@
+﻿namespace BCA.Challenge.CarAuction.API.Interfaces;
+
+public interface IEndpoint
+{
+    void MapEndpoints(WebApplication app);
+    void AddServices(IServiceCollection services);
+}
---
+++ BCA.Challenge.CarAuction/BCA.Challenge.CarAuction.API/Services/AuctionService.cs
@@ -0,0 +1,36 @@
+﻿using BCA.Challenge.CarAuction.API.Interfaces;
+using BCA.Challenge.CarAuction.API.Models;
+
+namespace BCA.Challenge.CarAuction.API.Services;
+
+public class AuctionService : IAuctionService
+{
+    public AuctionService()
+    {
+            
+    }
+    public void AddVehicle(Vehicle vehicle)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CloseAuction(Guid vehicleId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void PlaceBid(Guid vehicleId, decimal bidAmount, string bidder)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Vehicle> SearchVehicles(string type, string manufacturer, string model, int year)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void StartAuction(Guid vehicleId)
+    {
+        throw new NotImplementedException();
+    }
+}
