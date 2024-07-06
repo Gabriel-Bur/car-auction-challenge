bc5979c0b60b111abcff667d3deab8b257ab5c82
Gabriel Bur <gabriel.bur@outlook.com>
2024-07-06 23:32:47 +0100

    adding interface for auction service

---
+++ BCA.Challenge.CarAuction/BCA.Challenge.CarAuction.API/Interfaces/IAuctionService.cs
@@ -0,0 +1,12 @@
+﻿using BCA.Challenge.CarAuction.API.Models;
+
+namespace BCA.Challenge.CarAuction.API.Interfaces;
+
+public interface IAuctionService
+{
+    public void AddVehicle(Vehicle vehicle);
+    public IEnumerable<Vehicle> SearchVehicles(Vehicle vehicle);
+    public void StartAuction(Guid vehicleId);
+    public void PlaceBid(Guid vehicleId, decimal bidAmount, string bidder);
+    public void CloseAuction(Guid vehicleId);
+}
