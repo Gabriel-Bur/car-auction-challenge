3ea8cf625e9157157de99efa3b6d6b6d8860f046
Gabriel Bur <gabriel.bur@outlook.com>
2024-07-06 23:32:19 +0100

    adding models

---
+++ BCA.Challenge.CarAuction/BCA.Challenge.CarAuction.API/Models/Auction.cs
@@ -0,0 +1,9 @@
+﻿namespace BCA.Challenge.CarAuction.API.Models;
+
+public class Auction
+{
+    public Guid VehicleId { get; set; }
+    public bool IsActive { get; set; }
+    public decimal CurrentBid { get; set; }
+    public string CurrentBidder { get; set; } = string.Empty;
+}
---
+++ BCA.Challenge.CarAuction/BCA.Challenge.CarAuction.API/Models/Vehicle.cs
@@ -0,0 +1,29 @@
+﻿namespace BCA.Challenge.CarAuction.API.Models;
+public abstract class Vehicle
+{
+    public Guid Id { get; set; }
+    public string Manufacturer { get; set; } = string.Empty;
+    public abstract string Model { get; }
+    public int Year { get; set; }
+    public decimal StartingBid { get; set; }
+}
+public class Sedan : Vehicle
+{
+    public int NumberOfDoors { get; set; }
+
+    public override string Model => nameof(Sedan);
+}
+
+public class SUV : Vehicle
+{
+    public int NumberOfSeats { get; set; }
+
+    public override string Model => nameof(SUV);
+}
+
+public class Truck : Vehicle
+{
+    public double LoadCapacity { get; set; }
+
+    public override string Model => nameof(Truck);
+}
