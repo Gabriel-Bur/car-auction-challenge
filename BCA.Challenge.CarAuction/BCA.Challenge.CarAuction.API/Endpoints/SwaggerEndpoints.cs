93c7f28a5ecf4acd8c27767efed411c6654f74af
Gabriel Bur <gabriel.bur@outlook.com>
2024-07-07 20:20:55 +0100

    Adding swagger definition

---
+++ BCA.Challenge.CarAuction/BCA.Challenge.CarAuction.API/Endpoints/SwaggerEndpoints.cs
@@ -0,0 +1,33 @@
+﻿using BCA.Challenge.CarAuction.API.Interfaces;
+using Microsoft.OpenApi.Models;
+
+namespace BCA.Challenge.CarAuction.API.Endpoints
+{
+    public class SwaggerEndpoints : IEndpoint
+    {
+        public void AddServices(IServiceCollection services)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(s =>
+            {
+                s.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "CarAuction",
+                    Version = "v1",
+                });
+            });
+        }
+
+        public void MapEndpoints(WebApplication app)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(s =>
+                {
+                    s.SwaggerEndpoint("/swagger/v1/swagger.json", "CarAuctionAPI");
+                });
+            }
+        }
+    }
+}
