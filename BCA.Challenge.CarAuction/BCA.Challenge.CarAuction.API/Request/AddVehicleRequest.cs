325369d692630da927d48c7240d2f7fbd613b787
Gabriel Bur <gabriel.bur@outlook.com>
2024-07-07 20:21:12 +0100

    add request folder

---
+++ BCA.Challenge.CarAuction/BCA.Challenge.CarAuction.API/Request/AddVehicleRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace BCA.Challenge.CarAuction.API.Request;
+
+public class AddVehicleRequest
+{
+    public string Manufacturer { get; set; } = string.Empty;
+    public int Year { get; set; }
+    public decimal StartingBid { get; set; }
+    public int? NumberOfDoors { get; set; }     
+    public int? NumberOfSeats { get; set; }     
+    public double? LoadCapacity { get; set; }     
+}
---
+++ BCA.Challenge.CarAuction/BCA.Challenge.CarAuction.API/Request/GetFilterdVehicleRequest.cs
@@ -0,0 +1,11 @@
+﻿using BCA.Challenge.CarAuction.API.Models;
+
+namespace BCA.Challenge.CarAuction.API.Request;
+
+public class GetFilterdVehicleRequest
+{
+    public VehicleType? Type { get; set; }
+    public string Manufacturer { get; set; } = string.Empty;
+    public int? Year { get; set; }
+
+}
